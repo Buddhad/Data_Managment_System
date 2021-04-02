a4df88e485e4f79f12b8b13ac288e6f9d346fde5
Buddhadeb Chhetri <buddhadebchhetri6@gmail.com>
2021-04-02 19:53:31 +0530

    Contributation of my todo list

--- main.c
+++ main.c
@@ -14,25 +14,31 @@ void reuse();
 
 struct Students{
     int id;
-    int marks;
     char name[20];
+    char task[50];
 };
 
 int main(){
     time_t t;
     time(&t);
+    char name[50];
     ptr = fopen("CodetoContribute.txt","a");
-    fprintf(ptr,"\t---------DATABASES MANAGEMENT SYSTEM--------- \n");
-    fprintf(ptr,"\t-------------Code-to-Contribute-------------- \n");
-    fprintf(ptr,"These entries has been written at (date and time): %s \n", ctime(&t));
+    fprintf(ptr,"\t---------To-Do List--------- \n");
+    fprintf(ptr,"\t---------Made By Buddhadeb--------- \n");
+    printf("---------To-Do List--------- \n");
+    printf("---------Made By Buddhadeb------ \n");
+    printf("Enter Your name:  \n", name);
+    scanf("%s",&name);
+    printf(" \t  Hi! %s Welcome to the TO-DO List  \n",name);
+    
+    printf("----------------------------------------------------------------- \n ");
+    fprintf(ptr,"These entries has been written by %s at (date and time): %s \n",name, ctime(&t));
     fclose(ptr);
     // Date and time will be Printed when data will be written in text file
     reuse();
 }
 void reuse(){
     int num;
-    printf("---------DATABASES MANAGEMENT SYSTEM--------- \n");
-    printf("-------------Code-to-Contribute-------------- \n");
     printf("Press 1 to Continue and Any to Exit \n");
     scanf("%d",&num);
     if(num == 1)
@@ -47,28 +53,28 @@ int Data(int num){
     FILE *ptr;
     ptr = fopen("CodetoContribute.txt","a");
     int i, no_of_entry;
-
+    char name[50],task[100];
     printf("Please Enter the no. of Entries to be Stored \n");
     scanf("%d", &no_of_entry);
 
         for (i = 1; i <= no_of_entry; i++) {
 
-            printf("Enter the id of %d Student \n", i);
+            
+            printf("Enter the id of Task %d :  \n", i);
             scanf("%d", &Class10[i].id);
-            fprintf(ptr, "\tId - %d \n", Class10[i].id); // Writing all the contents to File
+            fprintf(ptr, "\tTask - %d \n", Class10[i].id); // Writing all the contents to File
 
-            printf("Enter the name of %d Student \n", i);
-            scanf("%s",&Class10[i].name);
-            fprintf(ptr, "\tName - %s \n", Class10[i].name);// Writing all the contents to File
-
-            printf("Enter the marks of %d Student \n", i);
-            scanf("%d", &Class10[i].marks);
-            fprintf(ptr, "\tMarks - %d \n", Class10[i].marks);// Writing all the contents to File
-
+            // printf("Enter Your name:  \n", i);
+            // scanf("%s",&Class10[i].name);
+            // fprintf(ptr, "\tName - %s \n", Class10[i].name);// Writing all the contents to File
+            printf("What you Want to do Today  :  \n");
+            scanf("%s",&Class10[i].task);
+            fprintf(ptr, "\tTask - %s \n", Class10[i].task);// Writing all the contents to File
+    
             fprintf(ptr,"-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-* \n ");
             printf("-------------------------------- \n ");
-        }
+            char a[50];
     reuse();
     return 0;
 }
-
+}
